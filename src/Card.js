e4e4826df460b591c901b015b4fe327a0472c485
juancastellano115 <juancastellano115@gmail.com>
2020-02-28 13:28:47 +0100

    lógica semi terminada

--- src/Card.js
+++ src/Card.js
@@ -7,15 +7,22 @@ class Card extends React.Component {
     super(props);
     this.Datos = props.datos;
   }
+  urlIcono = "https://developer.accuweather.com/sites/default/files/"
+
+  elegirIcono(icon){
+    
+  let icono = icon>10?icon:"0"+icon;
+  console.log(icono)
+    return(<img src={this.urlIcono+icono+"-s.png"} alt="icon"></img>)
+  }
 
   Datos = Object;
   render() {
     return (
       <Tilt className="card">
-        <h2>{this.Datos.city}</h2>
-        <i className="fas fa-cloud"></i>
-        <p>{this.Datos.max}</p>
-        <p>{this.Datos.min}</p>
+        <h2>{this.Datos.Day.IconPhrase}</h2>
+       {this.elegirIcono(this.Datos.Night.Icon)}
+    <p>{this.Datos.Temperature.Minimum.Value}º-{this.Datos.Temperature.Maximum.Value}º</p>
       </Tilt>
     );
   }
--- src/tarjetas.js
+++ src/tarjetas.js
@@ -1,55 +1,76 @@
-import React from 'react';
-import Card from './Card';
-
+import React from "react";
+import Card from "./Card";
 
 class Tarjetas extends React.Component {
+  url =
+    "http://api.openweathermap.org/data/2.5/weather?appid=19959cc224bcd5fca2667dda06dd0e66&units=metric";
+  accu = "http://dataservice.accuweather.com/forecasts/v1/daily/5day/";
+  geolocation =
+    "http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=QG7VY9lNLMTQXghXPt8bUjyGh3H34ztU&language=es-es&details=false&q=";
 
+  constructor() {
+    super();
+    this.state = { cargando: true };
+  }
+  componentDidMount() {
+    if ("geolocation" in navigator) {
+      //check Geolocation available
 
-    url = "http://api.openweathermap.org/data/2.5/weather?appid=19959cc224bcd5fca2667dda06dd0e66&units=metric"
-
-    constructor() {
-        super();
-        this.state = { cargando: true };
-    }
-    componentDidMount() {
-        if ("geolocation" in navigator) { //check Geolocation available
-            navigator.geolocation.getCurrentPosition((position) => {
-                this.setState({ geoLat: position.coords.latitude, geoLong: position.coords.longitude });
-                this.getWeatherData();
-            });
-        } else {
-            this.setState({ geoLat: 40.578626299999996, geoLong: -3.2488459 });
-            this.getWeatherData();
-        }
-
-    }
-
-    async getWeatherData() {
-        let response = await fetch(this.url + "&lat=" + this.state.geoLat + "&lon=" + this.state.geoLong);
-        if (response.ok) { // if HTTP-status is 200-299
-            // get the response body (the method explained below)
-            let json = await response.json();
-            let modeledObject = { city: json.name, max: json.main.temp_max, min: json.main.temp_min }
-            this.setState({ weatherData: modeledObject });
-
-            this.setState({ cargando: false });
-        }
-    }
-
-    render() {
-        return (
-            <div>
-            {this.state.cargando ? (
-              "cargando"
-            ) : (
-              <div>
-              <Card datos={this.state.weatherData} />
-              <Card datos={this.state.weatherData} />
-              </div>
-            )}
-          </div>
+      navigator.geolocation.getCurrentPosition(position => {
+        this.geolocation += encodeURI(
+          position.coords.latitude + "," + position.coords.longitude
         );
+        this.getWeatherData();
+      });
+    } else {
+      console.log("Error al obtener geolocalización");
     }
+  }
+
+  async getWeatherData() {
+    await fetch(this.geolocation).then(async res => {
+      if (res.ok) {
+        let datoslocalizacion = await res.json();
+        this.setState({
+          geo: datoslocalizacion.Key,
+          city: datoslocalizacion.LocalizedName
+        });
+      }
+    });
+    let url =
+      this.accu +
+      this.state.geo +
+      "?apikey=QG7VY9lNLMTQXghXPt8bUjyGh3H34ztU&language=es-es&details=false&metric=true";
+    console.log(url);
+    let response = await fetch(url);
+    if (response.ok) {
+      // if HTTP-status is 200-299
+      // get the response body (the method explained below)
+      let json = await response.json();
+      this.setState({ weatherData: json.DailyForecasts });
+      this.setState({ headline: json.Headline });
+
+      this.setState({ cargando: false });
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.cargando ? (
+          "cargando"
+        ) : (
+          <div>
+            <h1>{this.state.city}</h1>
+            <p>{this.state.headline.Text}</p>
+            {this.state.weatherData.map(tarjeta => (
+              <Card datos={tarjeta} />
+            ))}
+          </div>
+        )}
+      </div>
+    );
+  }
 }
 
 export default Tarjetas;
