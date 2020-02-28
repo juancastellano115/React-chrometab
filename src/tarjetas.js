841f48cfa68315be520f4187d71062a91ec077c9
diegobshots <blancolopez94@gmail.com>
2020-02-28 09:41:59 +0100

    diego 18/02

---
+++ src/tarjetas.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Card from './Card';
+import { render } from '@testing-library/react';
+
+
+class Tarjetas extends React.Component{
+   
+    
+
+    render(){
+        return(
+            <div></div>
+        );
+    }
+}
+
+export default Tarjetas;
