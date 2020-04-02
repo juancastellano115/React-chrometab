6fd882b98b420d45551713f5779d1985093f7e9f
juancastellano115 <juancastellano115@gmail.com>
2020-04-02 14:30:26 +0200

    Updates

---
+++ precache-manifest.1fe3bb6e390652303dd4b89e459b57e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee3d35a87392803e274c9b2307af4451",
+    "url": "/React-chrometab/index.html"
+  },
+  {
+    "revision": "73cb40a24c940e1890c1",
+    "url": "/React-chrometab/static/css/main.86626491.chunk.css"
+  },
+  {
+    "revision": "06e5a6f2c503e5ffb08b",
+    "url": "/React-chrometab/static/js/2.bdc300d5.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/React-chrometab/static/js/2.bdc300d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73cb40a24c940e1890c1",
+    "url": "/React-chrometab/static/js/main.e1fe9793.chunk.js"
+  },
+  {
+    "revision": "b1f17691939ad5ea6125",
+    "url": "/React-chrometab/static/js/runtime-main.dfabbe34.js"
+  }
+]);
